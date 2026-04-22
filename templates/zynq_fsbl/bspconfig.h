928224c5431c8827c370e0dc606bd01dc9de1477
Yichao Yu <yyc1992@gmail.com>
2026-04-21 20:17:26 -0400

    WIP: Adding zynq fsbl template

---
+++ templates/zynq_fsbl/bspconfig.h
@@ -0,0 +1,23 @@
+/*******************************************************************
+ * Copyright (c) 2010-2020 Xilinx, Inc. All Rights Reserved.
+ * Copyright (c) 2024 Advanced Micro Devices, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ *******************************************************************/
+
+#ifndef BSPCONFIG_H /* prevent circular inclusions */
+#define BSPCONFIG_H /* by using protection macros */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MICROBLAZE_PVR_NONE
+
+/* Definition for hard-float ABI */
+#define FPU_HARD_FLOAT_ABI_ENABLED 1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*end of __BSPCONFIG_H_*/
